f12d3d9ef3de25c3769002c45c632ed0126020d7
yisikawa <yisikawa@ma.medias.ne.jp>
2022-08-12 21:51:19 +0900

    add import/export

---
+++ IrrAssimpUtils.cpp
@@ -0,0 +1,15 @@
+#include "IrrAssimpUtils.h"
+
+#ifdef _MSC_VER
+#pragma comment (lib, "assimp-vc143-mtd.lib")
+#endif // _MSC_VER
+
+aiString irrToAssimpPath(const irr::io::path& path)
+{
+#ifdef _IRR_WCHAR_FILESYSTEM
+    irr::core::stringc rPath = path;
+    return aiString(rPath.c_str());
+#else
+    return aiString(path.c_str());
+#endif
+}
